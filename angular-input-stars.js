66d333f12beb20c15193f1f1501e8836f135c75f
Rafael Mello <ti.rcampanari@gmail.com>
2015-01-13 20:48:01 -0200

    Bugfix. Closes #2

--- angular-input-stars.js
+++ angular-input-stars.js
@@ -7,7 +7,7 @@ angular.module('app').directive('inputStars', function () {
         template:
         '<ul ng-class="listClass">' +
             '<li ng-repeat="item in items track by $index">' +
-                '<i  ng-class="getClass($index)" ng-click="setValue($index, $event)"></i>' +
+                '<i ng-mouseenter="paintStars($index)" ng-mouseleave="unpaintStars($index)" ng-class="getClass($index)" ng-click="setValue($index, $event)"></i>' +
             '</li>' +
         '</ul>',
         require: 'ngModel',
@@ -23,9 +23,9 @@ angular.module('app').directive('inputStars', function () {
 
         scope.items = new Array(+attrs.max);
         
-        var emptyIcon = attrs.iconEmpty || 'fa-star-o';
-        var fullIcon  = attrs.iconFull || 'fa-star';
-        var iconBase  = attrs.iconBase || 'fa fa-fw';
+        var emptyIcon   = attrs.iconEmpty || 'fa-star-o';
+        var fullIcon    = attrs.iconFull  || 'fa-star';
+        var iconBase    = attrs.iconBase  || 'fa fa-fw';
         scope.listClass = attrs.listClass || 'angular-input-stars';
 
         ngModelCtrl.$render = function () {
@@ -39,6 +39,36 @@ angular.module('app').directive('inputStars', function () {
             return index >= scope.last_value ? iconBase + ' '+ emptyIcon : iconBase + ' ' +fullIcon + ' active ';
 
         };
+
+        scope.unpaintStars = function () {
+
+            scope.paintStars( scope.last_value -1 );
+
+        }
+
+        scope.paintStars = function ($index) {
+            
+            var items = element.find('li').find('i');
+
+            for (var index = 0 ; index < items.length ; index++) {
+
+                var $star = angular.element(items[index]);
+
+                if ($index >= index) {
+
+                    $star.addClass(fullIcon);
+                    $star.addClass('active');
+                    $star.removeClass(emptyIcon);
+
+                } else {
+
+                    $star.removeClass(fullIcon);
+                    $star.removeClass('active');
+                    $star.addClass(emptyIcon);
+                
+                }
+            }
+        }
 
         scope.setValue = function(index, e) {
 
@@ -54,56 +84,6 @@ angular.module('app').directive('inputStars', function () {
         
         };
 
-        //Filling stars on mouseover
-        element.bind('mouseover', function(event) {
-            
-            var items = element.find('li').find('i');
-
-            for (var index = 0 ; index < items.length ; index++) {
-
-                var domNode = items[index];
-                var $star = angular.element(domNode);
-
-                if (event.pageX + 5 > domNode.getBoundingClientRect().left ) {
-
-                    $star.addClass(fullIcon);
-                    $star.addClass('active');
-                    $star.removeClass(emptyIcon);
-
-                } else {
-
-                    $star.removeClass(fullIcon);
-                    $star.removeClass('active');
-                    $star.addClass(emptyIcon);
-                
-                }
-
-            }
-        })
-        .bind('mouseout', function(event) {
-
-            var items = element.find('li').find('i');
-
-            for (var index = 0 ; index < items.length ; index++) {
-
-                var $star = angular.element(items[index]);
-                
-                if (scope.last_value > index){
-
-                    $star.addClass(fullIcon);
-                    $star.addClass('active');
-                    $star.removeClass(emptyIcon);
-                
-                } else {
-                
-                    $star.removeClass('active');
-                    $star.removeClass(fullIcon);
-                    $star.addClass(emptyIcon);
-                
-                }
-            }
-
-        });        
     }
 
 });
