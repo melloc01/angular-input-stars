4a86e0fa70232034b7b670fd2827add453579db9
anjorinjnr <tola.anjorin@gmail.com>
2015-03-01 21:40:57 -0500

    Optional param to make stars readonly.
    Sometimes you just want to render the stars and don't want any user interaction.
    I added an optional parameter to disable user interaction for this use-case.

--- angular-input-stars.js
+++ angular-input-stars.js
@@ -1,91 +1,103 @@
-angular.module('angular-input-stars',[])
+angular.module('angular-input-stars', [])
 
-.directive('inputStars', function () {
+    .directive('inputStars', function () {
 
-    var directive = {
+        var directive = {
 
-        restrict: 'EA',
-        replace: true,
-        template:
-        '<ul ng-class="listClass">' +
+            restrict: 'EA',
+            replace: true,
+            template: '<ul ng-class="listClass">' +
             '<li ng-mouseenter="paintStars($index)" ng-mouseleave="unpaintStars($index)" ng-repeat="item in items track by $index">' +
-                '<i  ng-class="getClass($index)" ng-click="setValue($index, $event)"></i>' +
+            '<i  ng-class="getClass($index)" ng-click="setValue($index, $event)"></i>' +
             '</li>' +
-        '</ul>',
-        require: 'ngModel',
-        scope:true,
+            '</ul>',
+            require: 'ngModel',
+            scope: {
+                readOnly: '=' //optional param to disable interaction
+            },
 
-        link: link
-
-    };
-
-    return directive;
-
-    function link (scope, element, attrs, ngModelCtrl) {
-
-        scope.items = new Array(+attrs.max);
-        
-        var emptyIcon   = attrs.iconEmpty || 'fa-star-o';
-        var fullIcon    = attrs.iconFull  || 'fa-star';
-        var iconBase    = attrs.iconBase  || 'fa fa-fw';
-        scope.listClass = attrs.listClass || 'angular-input-stars';
-
-        ngModelCtrl.$render = function () {
-
-            scope.last_value = ngModelCtrl.$viewValue;
-        
-        };
-
-        scope.getClass = function(index) {
-
-            return index >= scope.last_value ? iconBase + ' '+ emptyIcon : iconBase + ' ' +fullIcon + ' active ';
+            link: link
 
         };
 
-        scope.unpaintStars = function () {
+        return directive;
 
-            scope.paintStars( scope.last_value -1 );
+        function link(scope, element, attrs, ngModelCtrl) {
 
-        }
 
-        scope.paintStars = function ($index) {
-            
-            var items = element.find('li').find('i');
+            scope.items = new Array(+attrs.max);
 
-            for (var index = 0 ; index < items.length ; index++) {
+            var emptyIcon = attrs.iconEmpty || 'fa-star-o';
+            var fullIcon = attrs.iconFull || 'fa-star';
+            var iconBase = attrs.iconBase || 'fa fa-fw';
+            scope.listClass = attrs.listClass || 'angular-input-stars';
 
-                var $star = angular.element(items[index]);
+            ngModelCtrl.$render = function () {
 
-                if ( $index >= index ) {
+                scope.last_value = ngModelCtrl.$viewValue;
 
-                    $star.addClass(fullIcon);
-                    $star.addClass('active');
-                    $star.removeClass(emptyIcon);
+            };
 
-                } else {
+            scope.getClass = function (index) {
 
-                    $star.removeClass(fullIcon);
-                    $star.removeClass('active');
-                    $star.addClass(emptyIcon);
-                
+                return index >= scope.last_value ? iconBase + ' ' + emptyIcon : iconBase + ' ' + fullIcon + ' active ';
+
+            };
+
+            scope.unpaintStars = function () {
+
+                scope.paintStars(scope.last_value - 1);
+
+            };
+
+            scope.paintStars = function ($index) {
+                //ignore painting, if readonly
+                if (scope.readOnly) {
+                    return;
                 }
-            }
+                var items = element.find('li').find('i');
+
+                for (var index = 0; index < items.length; index++) {
+
+                    var $star = angular.element(items[index]);
+
+                    if ($index >= index) {
+
+                        $star.addClass(fullIcon);
+                        $star.addClass('active');
+                        $star.removeClass(emptyIcon);
+
+                    } else {
+
+                        $star.removeClass(fullIcon);
+                        $star.removeClass('active');
+                        $star.addClass(emptyIcon);
+
+                    }
+                }
+            };
+
+            scope.setValue = function (index, e) {
+                //ignore painting
+                if (scope.readOnly) {
+                    return;
+                }
+                if (scope.readOnly) {
+                    return;
+                }
+
+                var star = e.target;
+
+                if (e.pageX < star.getBoundingClientRect().left + star.offsetWidth / 2) {
+                    scope.last_value = index + 1;
+                } else {
+                    scope.last_value = index + 1;
+                }
+
+                ngModelCtrl.$setViewValue(scope.last_value);
+
+            };
+
         }
 
-        scope.setValue = function(index, e) {
-
-            var star = e.target;
-
-            if (e.pageX < star.getBoundingClientRect().left + star.offsetWidth / 2) {
-                    scope.last_value = index + 1;
-            } else {
-                    scope.last_value = index + 1;
-            }
-
-            ngModelCtrl.$setViewValue(scope.last_value);
-        
-        };
-
-    }
-
-});
+    });
