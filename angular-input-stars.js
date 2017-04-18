792ded50a43da9d2e7a09b981f278011650eec09
Rafael Mello Campanari <lab.rafamello@gmail.com>
2017-04-17 23:59:37 -0300

    Adding Half-icon support | closes #38 closes #18

--- angular-input-stars.js
+++ angular-input-stars.js
@@ -8,12 +8,14 @@ angular.module('angular-input-stars', [])
             restrict: 'EA',
             replace: true,
             template: '<ul ng-class="listClass">' +
-            '<li ng-touch="paintStars($index)" ng-mouseenter="paintStars($index, true)" ng-mouseleave="unpaintStars($index, false)" ng-repeat="item in items track by $index">' +
+            '<li ng-touch="paintStars($index)" ng-mouseenter="paintStars($index, true, $event)" ng-mouseleave="unpaintStars($index, false)" ng-repeat="item in items track by $index">' +
             '<i  ng-class="getClass($index)" ng-click="setValue($index, $event)"></i>' +
             '</li>' +
             '</ul>',
             require: 'ngModel',
-            scope: true,
+            scope:{
+                bindModel:'=ngModel'
+            },
             link: link
         };
 
@@ -21,6 +23,9 @@ angular.module('angular-input-stars', [])
 
         function link(scope, element, attrs, ngModelCtrl) {
             var computed = {
+                get allowHalf () {
+                    return typeof attrs.allowHalf == 'string' && attrs.allowHalf != 'false'
+                },
                 get readonly() {
                     return attrs.readonly != 'false' && (attrs.readonly || attrs.readonly === '');
                 },
@@ -54,12 +59,12 @@ angular.module('angular-input-stars', [])
 
             scope.getClass = function (index) {
                 var icon;
-      
+                
                 if (index >= scope.lastValue) {
                     icon = computed.iconBase + ' ' + computed.emptyIcon;
                 } else {
                     var isHalf = index + 0.5;
-                    if (isHalf === scope.lastValue) {
+                    if (computed.allowHalf && isHalf === scope.lastValue) {
                         icon = computed.iconBase + ' ' + computed.halfIcon + ' active ';
                     } else {
                         icon = computed.iconBase + ' ' + computed.fullIcon + ' active ';
@@ -72,7 +77,7 @@ angular.module('angular-input-stars', [])
                 scope.paintStars(scope.lastValue - 1, hover);
             };
 
-            scope.paintStars = function ($index, hover) {
+            scope.paintStars = function ($index, hover, $event) {
                 // ignore painting if readonly
                 if (computed.readonly) {
                     return;
@@ -82,25 +87,42 @@ angular.module('angular-input-stars', [])
 
                 for (var index = 0; index < items.length; index++) {
                     var $star = angular.element(items[index]);
+                    var classesToRemove;
+                    var classesToAdd;
 
                     if ($index >= index) {
-                        $star.removeClass(computed.emptyIcon);
-                        $star.addClass(computed.fullIcon);
-                        $star.addClass('active');
-                        $star.addClass(computed.iconHover);
+                        classesToRemove = [computed.emptyIcon, computed.halfIcon]
+                        classesToAdd = [computed.iconHover, computed.fullIcon, 'active']
                     } else {
-                        $star.removeClass(computed.fullIcon);
-                        $star.removeClass('active');
-                        $star.removeClass(computed.iconHover);
-                        $star.addClass(computed.emptyIcon);
+                        classesToRemove = [computed.fullIcon, computed.iconHover, computed.halfIcon, 'active']
 
+                        // isHalf
+                        if (computed.allowHalf && $index + 0.5 === index) {
+                            classesToAdd = [computed.halfIcon, 'active']
+                        } else {
+                            classesToAdd = [computed.emptyIcon]
+                        }
                     }
+
+                    $star.removeClass(classesToRemove.join(' '));
+                    $star.addClass(classesToAdd.join(' '));
                 }
 
                 if (! hover) {
                     items.removeClass(computed.iconHover);
                 }
             };
+
+            /**
+             * Returns whether the user is hovering the first half of the star or not.
+             * 
+             * @param {MouseEvent} e The mouse event.
+             * @param {HTMLLIElement} starDOMNode The scope "star" dom node.
+             * @returns {boolean}
+             */
+            function isHoveringFirstHalf (e, starDOMNode) {
+                return e.pageX < starDOMNode.getBoundingClientRect().left + starDOMNode.offsetWidth / 2
+            }
 
             scope.setValue = function (index, e) {
                 // ignore setting value if readonly
@@ -111,15 +133,15 @@ angular.module('angular-input-stars', [])
                 var star = e.target,
                     newValue;
 
-                if (e.pageX < star.getBoundingClientRect().left + star.offsetWidth / 2) {
-                    newValue = index + 1;
+                if (computed.allowHalf && isHoveringFirstHalf(e, star)) {
+                    newValue = index + 0.5;
                 } else {
                     newValue = index + 1;
                 }
 
                 // sets to 0 if the user clicks twice on the first "star"
                 // the user should be allowed to give a 0 score
-                if (newValue === scope.lastValue && newValue === 1) {
+                if (newValue === scope.lastValue) {
                     newValue = 0;
                 }
 
--- readme.md
+++ readme.md
@@ -21,32 +21,48 @@ Simple usage
 <input-stars max="5" ng-model="YourCtrl.property"></input-stars>
 ```
 
-You can customize the **base**, **empty**, **hover**, and **full** and  icon classes via attributes
+Customize the **base**, **empty**, **hover**, **half** and **full** and  icon classes via attributes
 ```html
-<input-stars max="5" icon-base="fa fa-fw" icon-empty="fa-circle-o" icon-hover="hover" icon-full="fa-circle" ng-model="YourCtrl.property"></input-stars>
+<input-stars
+    max="5"
+    allow-half
+    icon-base="fa fa-fw"
+    icon-empty="fa-star-o"
+    icon-half="fa-star-half-o"
+    icon-full="fa-star" 
+    icon-hover-full="fa-star" 
+    ng-model="YourCtrl.property"
+    ></input-stars>
 ```
 > Unlike icon-base, on icon-full, icon-hover and icon-empty you must specify only one class, but that is all you need : ]
 
-You can also customize or initalize the directive attributes using the `ng-attr-{attr}` directive thanks to [timkishkin](https://github.com/timkishkin) for pointing the need of a better clarification
+Customize or initalize the directive attributes using the `ng-attr-{attr}` directive thanks to [timkishkin](https://github.com/timkishkin) for pointing the need of a better clarification
 
 ```html
 <input-stars 
     ng-model="App.prop1" 
-    max="5" 
+    max="5"
     ng-attr-readonly="{{ enableReadonly || false }}" 
     ng-attr-icon-empty="{{ enableReadonly ? 'fa-twitter' : 'fa-circle-o' }}"
     ng-attr-icon-full="{{ enableReadonly ? 'fa-cog' : 'fa-twitter' }}"
 ></input-stars> value: {{App.prop1}}
 ```
 
-You can add a $rootScope function that will be called each time after a star is clicked by using the optional **onStarClick** attribute (thanks to [@whitef0x0](https://github.com/whitef0x0))
+Add a $rootScope function that will be called each time after a star is clicked by using the optional **onStarClick** attribute (thanks to [@whitef0x0](https://github.com/whitef0x0))
 ```html
 <input-stars max="5" on-star-click="runMyFunction()" ng-model="YourCtrl.property"></input-stars>
 ```
 
-You can customize the **list class** to whatever you want
+Customize the **list class** to whatever you want
 ```html
 <input-stars max="5" list-class="shiny-list" ng-model="YourCtrl.property"></input-stars>
+```
+
+Make it readonly thanks to [@anjorinjnr](https://github.com/anjorinjnr)
+```html
+<input-stars max="10" list-class="shiny-list" ng-model="YourCtrl.property" readonly ></input-stars>
+<input-stars max="10" list-class="shiny-list" ng-model="YourCtrl.property" readonly="true" ></input-stars>
+<input-stars max="10" list-class="shiny-list" ng-model="YourCtrl.property" readonly="readonly" ></input-stars>
 ```
 
 You can make it readonly thanks to [@anjorinjnr](https://github.com/anjorinjnr)
